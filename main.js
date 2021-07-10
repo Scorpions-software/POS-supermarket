61eba8ce57800253ec0b77c416e6361bfd2ab076
waseeld <dddwas12@gmail.com>
2021-07-10 07:13:26 +0300

    build vue files and run it in app

--- main.js
+++ main.js
@@ -1,12 +1,70 @@
 const { app, BrowserWindow } = require('electron')
 
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+
+http.createServer(function (request, response) {
+    console.log('request starting...');
+
+    var filePath = './src/frontend/dist' + request.url;
+    if (filePath == './src/frontend/dist/')
+        filePath = './src/frontend/dist/index.html';
+
+    var extname = path.extname(filePath);
+    var contentType = 'text/html';
+    switch (extname) {
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.json':
+            contentType = 'application/json';
+            break;
+        case '.png':
+            contentType = 'image/png';
+            break;      
+        case '.jpg':
+            contentType = 'image/jpg';
+            break;
+        case '.wav':
+            contentType = 'audio/wav';
+            break;
+    }
+
+    fs.readFile(filePath, function(error, content) {
+        console.log(filePath);
+        if (error) {
+            if(error.code == 'ENOENT'){
+                fs.readFile('./404.html', function(error, content) {
+                    response.writeHead(200, { 'Content-Type': contentType });
+                    response.end(content, 'utf-8');
+                });
+            }
+            else {
+                response.writeHead(500);
+                response.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
+                response.end(); 
+            }
+        }
+        else {
+            response.writeHead(200, { 'Content-Type': contentType });
+            response.end(content, 'utf-8');
+        }
+    });
+
+}).listen(8125);
+console.log('Server running at http://127.0.0.1:8125/');
+
 function createWindow() {
     const win = new BrowserWindow({
         width: 800,
         height: 600
     })
 
-    win.loadFile('./src/index.html')
+    win.loadURL("http://127.0.0.1:8125")
 }
 
 app.whenReady().then(() => {
